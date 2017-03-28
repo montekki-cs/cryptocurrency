a906034bb9ed5907334aeab1b33be8ee4aa51c1f
Fedor Sakharov <teo.montekki@yandex.ru>
2017-03-28 18:45:19 +0300

    Adds CompliantNode.java implementation

--- assignment2starterCode/CompliantNode.java
+++ assignment2starterCode/CompliantNode.java
@@ -1,26 +1,51 @@
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.HashSet;
 
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
 
+    private boolean[] followees;
+    private Set<Transaction> pendingTransactions;
+    private ArrayList<Integer[]> candidates;
+
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
-        // IMPLEMENT THIS
+         candidates = new ArrayList<Integer[]>();
     }
 
     public void setFollowees(boolean[] followees) {
-        // IMPLEMENT THIS
+        this.followees = new boolean[followees.length];
+
+        for (int i = 0; i < followees.length; i++) {
+            this.followees[i] = followees[i];
+        }
+
+        return;
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
-        // IMPLEMENT THIS
+        this.pendingTransactions = new HashSet<Transaction>();
+
+        for (Transaction tx : pendingTransactions) {
+            this.pendingTransactions.add(tx);
+        }
     }
 
     public Set<Transaction> sendToFollowers() {
-        // IMPLEMENT THIS
+        Set<Transaction> res = new HashSet<Transaction>();
+      
+        for (Transaction tx : pendingTransactions) {
+            res.add(tx);
+        }
+
+        return res;
     }
 
     public void receiveFromFollowees(Set<Candidate> candidates) {
-        // IMPLEMENT THIS
+        for (Candidate candidate : candidates) {
+            pendingTransactions.add(candidate.tx);
+        }
+
+        return;
     }
 }
