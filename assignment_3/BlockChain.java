721034f1add2b1e3ab20a119a6838d11e7d26ca6
Fedor Sakharov <teo.montekki@yandex.ru>
2017-03-31 21:08:56 +0300

    Adds assignment_3 code

--- assignment_3/BlockChain.java
+++ assignment_3/BlockChain.java
@@ -36,30 +36,55 @@ public class BlockChain {
       }
    }
 
+   private TxHandler txHandler;
+   private TransactionPool txPool;
+   private BlockNode blockRoot;
+   private BlockNode maxHeightBlock;
+   private HashMap<byte[], BlockNode> allBlocks;
+   private ArrayList<BlockNode> heads;
+   private int height;
+
    /* create an empty block chain with just a genesis block.
     * Assume genesis block is a valid block
     */
    public BlockChain(Block genesisBlock) {
-      // IMPLEMENT THIS
+      UTXOPool newPool = new UTXOPool();
+
+      for (int i = 0; i < genesisBlock.getCoinbase().numOutputs(); i++) {
+         UTXO utxo = new UTXO(genesisBlock.getCoinbase().getHash(), i);
+         newPool.addUTXO(utxo, genesisBlock.getCoinbase().getOutput(i));
+      }
+
+      heads = new ArrayList<BlockNode>();
+      allBlocks = new HashMap<byte[], BlockNode>();
+      txHandler = new TxHandler(newPool);
+      blockRoot = new BlockNode(genesisBlock, null, newPool);
+      heads.add(blockRoot);
+
+      allBlocks.put(genesisBlock.getHash(), blockRoot);
+      maxHeightBlock = blockRoot;
+      txPool = new TransactionPool();
+
+      height = 1;
    }
 
    /* Get the maximum height block
-    */
+   */
    public Block getMaxHeightBlock() {
-      // IMPLEMENT THIS
+      return maxHeightBlock.b;
    }
-   
+
    /* Get the UTXOPool for mining a new block on top of 
     * max height block
     */
    public UTXOPool getMaxHeightUTXOPool() {
-      // IMPLEMENT THIS
+      return maxHeightBlock.getUTXOPoolCopy();
    }
-   
+
    /* Get the transaction pool to mine a new block
-    */
+   */
    public TransactionPool getTransactionPool() {
-      // IMPLEMENT THIS
+      return txPool;
    }
 
    /* Add a block to block chain if it is valid.
@@ -71,12 +96,77 @@ public class BlockChain {
     * Return true of block is successfully added
     */
    public boolean addBlock(Block b) {
-       // IMPLEMENT THIS
+      if (b.getPrevBlockHash() == null) {
+         return false;
+      }
+
+      if (allBlocks.containsKey(b.getPrevBlockHash()) == false) {
+         return false;
+      }
+
+      BlockNode prevNode = allBlocks.get(b.getPrevBlockHash());
+
+      if (prevNode == null) {
+         return false;
+      }
+
+      if (prevNode.getUTXOPoolCopy() == null) {
+         return false;
+      }
+
+      TxHandler handler = new TxHandler(prevNode.getUTXOPoolCopy());
+
+      Transaction[] transactions = new Transaction[b.getTransactions().size()];
+
+      b.getTransactions().toArray(transactions);
+
+      Transaction[] res = handler.handleTxs(transactions);
+
+      if (res.length != transactions.length) {
+         return false;
+      }
+
+      ArrayList<Transaction> txs = b.getTransactions();
+      for (Transaction tx : txs) {
+         txPool.removeTransaction(tx.getHash());
+      }
+
+
+      UTXOPool newPool = handler.getUTXOPool();
+      Transaction coinbase = b.getCoinbase();
+
+      for (int i = 0; i < coinbase.numOutputs(); i++) {
+         UTXO utxo = new UTXO(coinbase.getHash(), i);
+         newPool.addUTXO(utxo, coinbase.getOutput(0));
+      }
+
+      BlockNode newNode = new BlockNode(b, prevNode, newPool);
+      allBlocks.put(b.getHash(), newNode);
+
+      if (newNode.height > height) {
+         height = prevNode.height;
+         maxHeightBlock = newNode;
+      }
+
+      if (height - heads.get(0).height > CUT_OFF_AGE) {
+         ArrayList<BlockNode> newHeads = new ArrayList<BlockNode>();
+         for (BlockNode bHeads : heads) {
+            for (BlockNode bChild : bHeads.children) {
+               newHeads.add(bChild);
+            }
+            allBlocks.remove(bHeads.b.getHash());
+         }
+
+         heads = newHeads;
+      }
+
+
+      return true;
    }
 
    /* Add a transaction in transaction pool
-    */
+   */
    public void addTransaction(Transaction tx) {
-      // IMPLEMENT THIS
+      txPool.addTransaction(tx);
    }
 }
---
+++ assignment_3/TxHandler.java
@@ -0,0 +1,150 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class TxHandler {
+
+	private UTXOPool utxoPool;
+
+	/* Creates a public ledger whose current UTXOPool (collection of unspent 
+	 * transaction outputs) is utxoPool. This should make a defensive copy of 
+	 * utxoPool by using the UTXOPool(UTXOPool uPool) constructor.
+	 */
+	public TxHandler(UTXOPool utxoPool) {
+		this.utxoPool = new UTXOPool(utxoPool);
+	}
+
+	/* Returns true if 
+	 * (1) all outputs claimed by tx are in the current UTXO pool, 
+	 * (2) the signatures on each input of tx are valid, 
+	 * (3) no UTXO is claimed multiple times by tx, 
+	 * (4) all of tx’s output values are non-negative, and
+	 * (5) the sum of tx’s input values is greater than or equal to the sum of   
+	        its output values;
+	   and false otherwise.
+	 */
+
+	public boolean isValidTx(Transaction tx) {
+		double input = 0.0, output = 0.0;
+
+		for (int i = 0; i < tx.numInputs(); i++) {
+			Transaction.Input ti = tx.getInput(i);
+			UTXO utxo = new UTXO(ti.prevTxHash, ti.outputIndex);
+
+			if (utxoPool.contains(utxo) == false) {
+				return false;
+			}
+
+			for (int j = i + 1; j < tx.numInputs(); j++) {
+				Transaction.Input ti2 = tx.getInput(j);
+				UTXO utxo2 = new UTXO(ti2.prevTxHash, ti2.outputIndex);
+
+				if (utxo2.equals(utxo)) {
+					return false;
+				}
+			}
+
+			Transaction.Output to = utxoPool.getTxOutput(utxo);
+
+			input += to.value;
+
+			if (to.address.verifySignature(tx.getRawDataToSign(i), ti.signature) != true) {
+				return false;
+			}
+		}
+
+		for (int i = 0; i < tx.numOutputs(); i++) {
+			Transaction.Output to = tx.getOutput(i);
+
+			if (to.value < 0) {
+				return false;
+			}
+
+			output += to.value;
+		}
+
+		if (output > input) {
+			return false;
+		}
+
+		return true;
+	}
+	private boolean inPool(Transaction tx) {
+		ArrayList<Transaction.Input> inputs = tx.getInputs();
+		Transaction.Input in;
+		UTXO ut;
+		for (int i = 0; i < inputs.size(); i++) {
+			in = inputs.get(i);
+			ut = new UTXO(in.prevTxHash, in.outputIndex);
+			if (!utxoPool.contains(ut))
+				return false;
+		}
+		return true;
+	}
+
+	/* Handles each epoch by receiving an unordered array of proposed 
+	 * transactions, checking each transaction for correctness, 
+	 * returning a mutually valid array of accepted transactions, 
+	 * and updating the current UTXO pool as appropriate.
+	 */
+	public Transaction[] handleTxs(Transaction[] possibleTxs) {
+		Transaction[] tempArray = new Transaction[possibleTxs.length];
+		int tempArrayLength = possibleTxs.length;
+		int foundNum = 0;
+
+		for (int i = 0; i < tempArrayLength; i++) {
+			tempArray[i] = possibleTxs[i];
+		}
+
+		while (true) {
+			boolean newFound = false;
+
+			for (int i = 0; i < tempArrayLength; i++) {
+				if (tempArray[i] == null) {
+					continue;
+				}
+
+				if (inPool(tempArray[i]) == true && isValidTx(tempArray[i]) == true) {
+
+					for (int j = 0; j < tempArray[i].numInputs(); j++) {
+						Transaction.Input ti = tempArray[i].getInput(j);
+						UTXO utxo = new UTXO(ti.prevTxHash, ti.outputIndex);
+
+						utxoPool.removeUTXO(utxo);
+					}
+
+					for (int j = 0; j < tempArray[i].numOutputs(); j++) {
+						Transaction.Output to = tempArray[i].getOutput(j);
+
+						UTXO utxo = new UTXO(tempArray[i].getHash(), j);
+						utxoPool.addUTXO(utxo, to);
+					}
+
+					tempArray[i] = null;
+					foundNum++;
+					newFound = true;
+				}
+			}
+
+			if (newFound == false) {
+				break;
+			}
+		}
+
+		Transaction[] result = new Transaction[foundNum];
+
+		for (int i = 0, j = 0; i < tempArrayLength; i++) {
+			if (tempArray[i] == null) {
+				result[j] = possibleTxs[i];
+				j++;
+			}
+		}
+
+		return result;
+	}
+
+	public UTXOPool getUTXOPool() {
+		UTXOPool ret = new UTXOPool(utxoPool);
+
+		return ret;
+	}
+}
